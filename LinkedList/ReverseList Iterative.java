05f394d05417093ee5b5fa5c44e80f4dee9c52aa
swara12262 <sawant12262@gmail.com>
2020-07-24 18:30:31 +0530

    Create ReverseList Iterative.java

---
+++ LinkedList/ReverseList Iterative.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode cur=head, prev=null, next=null;
+        
+        while(cur!=null)
+        {
+           next=cur.next;
+            cur.next=prev;
+            prev=cur;
+            cur=next;
+        }
+        return prev;
+    }
+}
