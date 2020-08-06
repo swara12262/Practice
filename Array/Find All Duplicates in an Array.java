0074d65756b45d210e07de22bf2ac74971504126
swara12262 <sawant12262@gmail.com>
2020-08-06 21:21:18 +0530

    Create   Find All Duplicates in an Array.java
    Time complexity:O(n)
    Space complexity: O(1)

---
+++ Array/Find All Duplicates in an Array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        
+        List<Integer> res=new ArrayList();
+        for(int i: nums){
+            if(nums[Math.abs(i)-1]<0)
+                res.add(Math.abs(i));
+            nums[Math.abs(i)-1]*=-1;
+        }
+        return res;
+    }
+}
