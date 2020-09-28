ba7e6dc89217340902b0ec494ef9e92f3134af2e
swara12262 <sawant12262@gmail.com>
2020-09-28 07:54:35 +0530

    Create 122. Best Time to Buy and Sell Stock II.java
    Time complexity : O(n) Single pass.
    
    Space complexity : O(1) Constant space required.

---
+++ Mathematical or Trivial/122. Best Time to Buy and Sell Stock II.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+     //peak valley approach
+        int valley=prices[0];
+        int peak=prices[0];
+        int i=0;
+        int maxProfit=0;
+        while(i<prices.length-1){
+            
+            while(i<prices.length-1 && prices[i]>=prices[i+1])i++;
+            
+            valley=prices[i];
+            while(i<prices.length-1 && prices[i]<=prices[i+1])i++;
+            
+            peak=prices[i];
+            maxProfit+=peak-valley;
+            
+        }
+        return maxProfit;
+        
+        
+    }
+}
