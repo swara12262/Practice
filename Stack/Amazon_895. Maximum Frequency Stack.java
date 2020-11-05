a5e938305c55620de4c84597a0e121f4474ab634
swara12262 <sawant12262@gmail.com>
2020-11-05 18:33:05 +0530

    Create Amazon_895. Maximum Frequency Stack.java
    Time Complexity: O(1) for both push and pop operations.
    
    Space Complexity: O(N), where N is the number of elements in the FreqStack.

---
+++ Stack/Amazon_895. Maximum Frequency Stack.java
@@ -0,0 +1,31 @@
+class FreqStack {
+    Map<Integer, Integer> freq;
+    Map<Integer, Stack<Integer>> group;
+    int maxfreq;
+
+    public FreqStack() {
+        freq = new HashMap();
+        group = new HashMap();
+        maxfreq = 0;
+    }
+
+    public void push(int x) {
+        int f = freq.getOrDefault(x, 0) + 1;
+        freq.put(x, f);
+        if (f > maxfreq)
+            maxfreq = f;
+
+        //group.computeIfAbsent(f, z-> new Stack()).push(x);
+         Stack tmpstk = group.getOrDefault(f, new Stack());
+         tmpstk.push(x);
+         group.put(f,tmpstk);
+    }
+
+    public int pop() {
+        int x = group.get(maxfreq).pop();
+        freq.put(x, freq.get(x) - 1);
+        if (group.get(maxfreq).size() == 0)
+            maxfreq--;
+        return x;
+    }
+}
