ed4226ba1ddcecb0d88e492ca75899469a94abaf
swara12262 <sawant12262@gmail.com>
2020-11-13 16:51:48 +0530

    Create Amazon_Remove Duplicates from Sorted Array.java
    Time complexity: O(n)
    Space complexity: O(1)

---
+++ Array/Amazon_Remove Duplicates from Sorted Array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+     
+        if(nums.length==0)
+            return 0;
+        //to keep track of unique elements in nums
+        int j=0;
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]!=nums[i+1])
+                nums[j++]=nums[i];
+        }
+        nums[j++]=nums[nums.length-1];
+        
+        return j;
+        
+    }
+}
