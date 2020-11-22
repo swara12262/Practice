d1a86d808c2291d897aabee78c996340d2e73b36
swara12262 <sawant12262@gmail.com>
2020-11-22 15:27:39 +0530

    Create 392. Is Subsequence.java

---
+++ Array/392. Is Subsequence.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+     
+        int l= 0, r=0;
+        while(l<s.length() && r<t.length()){
+            if(s.charAt(l)==t.charAt(r))
+            {
+                l++;
+            }
+                r++;
+        }
+        return l==s.length();
+    }
+}
